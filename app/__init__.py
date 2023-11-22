043c0f91b64923e942b7d0ff2ae6e250a57d25ca
Tyson Franks <tjfranks02@hotmail.com>
2023-11-22 20:51:06 +1100

    Configured signin route to forward requests to auth microservices

--- app/__init__.py
+++ app/__init__.py
@@ -4,32 +4,36 @@ from flask import Flask
 
 
 def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-      SECRET_KEY="dev", # Change to something less obvious/randomised in production
-      DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
-    )
+  # Load in environment variables file
+  from dotenv import load_dotenv
+  load_dotenv()
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+  # Create and configure the app
+  app = Flask(__name__, instance_relative_config=True)
+  app.config.from_mapping(
+    SECRET_KEY="dev", # Change to something less obvious/randomised in production
+    DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
+  )
 
-    # ensure the instance folder exists
-    try:
-      os.makedirs(app.instance_path)
-    except OSError:
-      pass
-    
-    # Register functions related to managing the database
-    from . import db
-    db.init_app(app)
+  if test_config is None:
+      # Load the instance config, if it exists, when not testing
+      app.config.from_pyfile("config.py", silent=True)
+  else:
+      # Load the test config if passed in
+      app.config.from_mapping(test_config)
 
-    # Register blueprint which bundles several auth views
-    from . import auth
-    app.register_blueprint(auth.bp)
+  # Ensure the instance folder exists
+  try:
+    os.makedirs(app.instance_path)
+  except OSError:
+    pass
+  
+  # Register functions related to managing the database
+  from . import db
+  db.init_app(app)
 
-    return app
+  # Register blueprint which bundles several auth views
+  from . import auth
+  app.register_blueprint(auth.bp)
+
+  return app
--- app/auth.py
+++ app/auth.py
@@ -1,64 +1,17 @@
-import functools
+import os
+from flask import Blueprint, request
+import requests
 
-from flask import (
-    Blueprint, flash, g, redirect, render_template, request, session, url_for
-)
-from werkzeug.security import check_password_hash, generate_password_hash
+bp = Blueprint("auth", __name__, url_prefix="/auth")
 
-from app.db import get_db
+@bp.route("/signin", methods=["POST"])
+def signIn():
+  print(request.json)
 
-bp = Blueprint('auth', __name__, url_prefix='/auth')
+  # Forward the request to the auth service
+  sign_in_res = requests.post(
+    os.environ.get("AUTH_SERVICE_URL") + "/users/signin",
+    json=request.json
+  )
 
-# Notice you define the route on the blueprint!
-@bp.route('/register', methods=('GET', 'POST'))
-def register():
-  if request.method == 'POST':
-    username = request.form['username']
-    password = request.form['password']
-    db = get_db()
-    error = None
-
-    if not username:
-      error = 'Username is required.'
-    elif not password:
-      error = 'Password is required.'
-
-    if error is None:
-      try:
-        db.execute(
-            "INSERT INTO user (username, password) VALUES (?, ?)",
-            (username, generate_password_hash(password)),
-        )
-        db.commit()
-      except db.IntegrityError:
-        error = f"User {username} is already registered."
-      else:
-        return redirect(url_for("auth.login"))
-    flash(error)
-
-  return render_template('auth/register.html')
-
-@bp.route('/login', methods=('GET', 'POST'))
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        db = get_db()
-        error = None
-        user = db.execute(
-            'SELECT * FROM user WHERE username = ?', (username,)
-        ).fetchone()
-
-        if user is None:
-            error = 'Incorrect username.'
-        elif not check_password_hash(user['password'], password):
-            error = 'Incorrect password.'
-
-        if error is None:
-            session.clear()
-            session['user_id'] = user['id']
-            return redirect(url_for('index'))
-
-        flash(error)
-
-    return render_template('auth/login.html')
+  return sign_in_res.content, sign_in_res.status_code, sign_in_res.headers.items()
